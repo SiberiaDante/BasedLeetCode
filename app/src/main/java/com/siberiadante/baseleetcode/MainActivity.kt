6af635bde121e6f8f8b595ad014c13114b3ed818
SiberiaDante <994537867@qq.com>
2018-04-09 09:40:09 +0800

    9 判断一个整数是否是回文数。不能使用辅助空间

--- app/src/main/java/com/siberiadante/baseleetcode/MainActivity.kt
+++ app/src/main/java/com/siberiadante/baseleetcode/MainActivity.kt
@@ -3,7 +3,7 @@ package com.siberiadante.baseleetcode
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.siberiadante.baseleetcode.leetcode007.Kotlin007
+import com.siberiadante.baseleetcode.leetcode009.Kotlin009
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //        var reverse = Java007.reverse(1112)
-        var k = Kotlin007()
-        var reverse = k.reverse(1234567891)
-        Log.d("Main", "--------------" + reverse)
+//        var k = Kotlin007()
+//        var reverse = k.reverse(1234567891)
+        var kotlin009 = Kotlin009()
+        var reverse = kotlin009.isPalindrome(1234124321)
+        Log.d("Main", "-------result-------" + reverse)
     }
 }
